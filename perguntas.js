626f7c0f4133425f5dc176642e389758a45fc6dd
Luisa Rocha Vargas <luisa.vargas@escola.pr.gov.br>
2025-06-30 10:46:59 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+  'Programação',
+  'O que é Phyton?',
+  'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+  'Geografia',
+  'Qual a capital da França?',
+  'A capital da França é Paris'
+)
+
+criaCartao(
+  'Programação',
+  'O que é uma função?',
+  'Função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+  'Lingua inglesa',
+  'Como se diz oi em inglês?',
+  'Oi se diz HI em inglês'
+)
+
+criaCartao(
+  'Biologia',
+  'Qual o maior osso do corpo humano?',
+  'O maior osso do corpo humano é o fêmur'
+)
+
+criaCartao(
+  'Química',
+  'O que é um átomo?',
+  'É a menor unidade de um elemento químico que ainda mantém suas propriedades'
+)
